b2d80d5741fd1c4c6cedf5d11e47aff54561e3a5
kuanchiliao1 <kuanchiliao@gmail.com>
2023-02-20 21:30:45 -0500

    Add code for watchlist

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 const apiKey = "f67d1b01"
 const moviesContainer = document.getElementById("movies-container")
+const searchForm = document.getElementById("search-form")
 
-let input = "guardian"
 
-// fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${input}`)
-//   .then(response => response.json())
-//   .then(data => {
-//     data.Search.forEach(movie => {
-//       const movieId = movie.imdbID
-//       fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movieId}`)
-//       .then(response => response.json())
-//       .then(movieData => {
-//         moviesContainer.innerHTML += getMovieHtml(movieData)
-//         const addBtn = document.getElementById(movieId)
-//         console.log(addBtn)
-//         setTimeout(() => {
-//           addBtn.addEventListener("click", () => {
-//             console.log("testing")
-//           })
-//         }, 5000)
-//       })
-//     })
-
-//     // console.log(getMovieHtml(data.Search[0]))
-    
-//   })
+// if we're on the watchlist page, run this code
+const moviesContainerWatchlist = document.getElementById("movies-container--watchlist")
+if (moviesContainerWatchlist) {
+  // Get the movies from local storage
+  const movies = JSON.parse(localStorage.getItem("movies"))
+  // Iterate through the movies
+  movies.forEach(movie => {
+    // Generate the HTML and add it to the DOM
+    moviesContainerWatchlist.insertAdjacentHTML("beforeend", getMovieHtml(movie))
+  })
+}
 
 // Wait until content is loaded before running the code
-document.addEventListener("DOMContentLoaded", () => {
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  // Clear the movies container
+  moviesContainer.innerHTML = ""
+
+  const searchInput = document.getElementById("search-input").value
+  console.log(searchInput)
   // Grab a list of movie objects from the OMDB API
-  fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${input}`)
+  fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchInput}`)
     .then(response => response.json())
     .then(data => {
       // Iterate through the list of movies
@@ -41,19 +36,42 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(movieData => {
           // Generate the HTML and add it to the DOM
           moviesContainer.insertAdjacentHTML("beforeend", getMovieHtml(movieData)) 
-          
           const addBtn = document.getElementById(movieId)
           addBtn.addEventListener("click", () => {
-            if (!localStorage.getItem(movieId)) {
-              localStorage.setItem(movieId, JSON.stringify(movieData))
-            }
-            console.log(movieId)
+            addToLocalStorage(movieData)
+            console.log(JSON.parse(localStorage.getItem("movies")).map(movie => movie.Title))
           })
-            
         })
       })
     })
 })
+
+// Pseudocode
+// - Check if there is anything in local storage
+//   - If there is nothing in local storage, add the movie to local storage as an array
+//   - If there is something in local storage, check if the movie is already in local storage
+//     - If the movie is already in local storage, do nothing
+//     - If the movie is not in local storage, add the movie to local storage
+function addToLocalStorage(movieData) {
+  // const storageArray = [JSON.parse(localStorage.getItem("movies")] || [])
+  // (storageArray === []) ? localStorage.setItem("movies", JSON.stringify([movieData])) : storageArray.push(movieData)
+  // console.log(storageArray)
+  
+  // If there is nothing in local storage, add the movie to local storage as an array
+  if (!localStorage.getItem("movies")) {
+    localStorage.setItem("movies", JSON.stringify([movieData]))
+  } else {
+    if (isMovieInLocalStorage(movieData)) return
+    const storageArray = JSON.parse(localStorage.getItem("movies"))
+    storageArray.push(movieData)
+    localStorage.setItem("movies", JSON.stringify(storageArray))
+  }
+}
+
+function isMovieInLocalStorage(movieData) {
+  const storageArray = JSON.parse(localStorage.getItem("movies"))
+  return storageArray.some(movie => movie.imdbID === movieData.imdbID)
+}
 
 
 // Steps:
@@ -69,16 +87,14 @@ function getMovieHtml(movieData) {
   return `
   <div class="movie-container">
     <div class="movie-img-info-container flex">
-      <div class="movie-img-container">
         <img src="${Poster}" alt="picture of movie">
-      </div>
       <div class="movie-info-container">
         <h2 class="movie-title">${Title}
           <span class="rating">⭐${imdbRating}</span>
         </h2>
         <p class="movie-time">${Runtime}</p>
         <p class="movie-genre">${Genre}</p>
-        <button class="movie-add-btn" id="${imdbID}">+ Watchlist</button>
+        <button class="movie-add-btn" id="${imdbID}">➕ Watchlist</button>
       </div>
     </div>
     <p class="movie-description">${Plot}</p>
